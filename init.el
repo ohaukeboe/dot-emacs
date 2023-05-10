66a5de93a1e612a0ee233e99268cc7b05c3fe605
Oskar Haukebøe <ohaukeboe@pm.me>
2023-05-10 11:50:50 +0200

    Projectile and stuff

--- init.el
+++ init.el
@@ -1,17 +1,6 @@
-;;; Package --- Summary
+;;(require 'org)
+(org-babel-load-file
+ (expand-file-name "configuration.org" user-emacs-directory))
 
-;;; Commentary:
-;; Emacs init file responsible for either loading a pre-compiled configuration
-;; file or tangling and loading a literate org configuration file.
 
-;; Don't attempt to find/apply special file handlers to files loaded during
-;; startup.
-(let ((file-name-handler-alist nil))
-  ;; If config is pre-compiled, then load that
-  (if (file-exists-p (expand-file-name "configuration.elc" user-emacs-directory))
-      (load-file (expand-file-name "configuration.elc" user-emacs-directory))
-    ;; Otherwise use org-babel to tangle and load the configuration
-    (require 'org)
-    (org-babel-load-file (expand-file-name "configuration.org" user-emacs-directory))))
 
-;;; init.el ends here
