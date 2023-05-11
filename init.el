056d56e7d9ad126229cb78fafa4afb20342c6d4d
Oskar Haukebøe <ohaukeboe@pm.me>
2023-05-11 23:21:35 +0200

    It's tangled again

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
-;;(require 'org)
-(org-babel-load-file
- (expand-file-name "configuration.org" user-emacs-directory))
+;; Load the configuration from the org file if it exists, otherwise load the
+;; already tangled file.
+(let ((org-file (expand-file-name (locate-user-emacs-file "configuration.org")))
+      (el-file (expand-file-name (locate-user-emacs-file "configuration.el"))))
+  (if (file-exists-p el-file)
+      (load-file el-file)
+    (when (file-exists-p org-file)
+      (require 'org)
+      (message "Tangling %s" org-file)
+      (org-babel-load-file org-file))))
