0a95f4c31922a4d683d5ce62091b553f343fccbe
Oskar Haukebøe <ohaukeboe@pm.me>
2024-04-03 14:37:58 +0200

    Smaller fontsize

--- init.el
+++ init.el
@@ -10,21 +10,3 @@
     ;; Otherwise use org-babel to tangle and load the configuration
     (require 'org)
     (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((lsp-csharp-solution-file . "~/knowit/assman/AssetManager/Equipit2/Equipit2.sln")
-     (lsp-csharp-solution-file . "Equipit2/Equipit2.sln")
-     (lsp-csharp-solution-file . ~/knowit/assman/AssetManager/Equipit2/Equipit2.sln)
-     (lsp-csharp-solution-file . Equipit2/Equipit2.sln))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Roboto Mono" :height 130))))
- '(fixed-pitch ((t (:family "Roboto Mono" :height 0.9))))
- '(variable-pitch ((t (:family "Roboto Serif" :height 1.3)))))
