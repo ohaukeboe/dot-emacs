2b4901aff19b6ee1c9c93796eaed45ca1f52f1d4
Oskar Haukebøe <ohaukeboe@pm.me>
2025-01-17 14:50:43 +0100

    fix plists for emacs

--- home.nix
+++ home.nix
@@ -30,7 +30,9 @@
   	      copilot
   	      jinx
   	    ];
-  	  });
+      }).overrideAttrs (oldAttrs: {
+          LSP_USE_PLISTS = "true";
+      });
   	};
 
     chromium = {
@@ -283,7 +285,7 @@
   home.sessionVariables = {
     EDITOR = "vim";
     DOTNET_ROOT = "${pkgs.dotnet-sdk_8}";
-    LSP_USE_PLISTS= "true";
+    # LSP_USE_PLISTS= "true";
   };
 
   # # Enable lorri for easy development environment
