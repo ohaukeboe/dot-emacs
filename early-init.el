248bd0e0639f659337e20e97c696148af51640d0
Oskar Haukebøe <ohaukeboe@pm.me>
2025-01-18 20:19:27 +0100

    Revert "move plists to early-init.el"
    This reverts commit 632d1d3c17876ff124e601203645810c629898bf.

--- early-init.el
+++
@@ -1,7 +0,0 @@
-;;; early-init.el --- Early initialization
-
-;;; Code:
-(setenv "LSP_USE_PLISTS" "true")
-
-(provide 'early-init)
-;;; early-init.el ends here
--- home.nix
+++ home.nix
@@ -30,6 +30,8 @@
   	      copilot
   	      jinx
   	    ];
+      }).overrideAttrs (oldAttrs: {
+          LSP_USE_PLISTS = "true";
       });
   	};
 
@@ -282,6 +284,7 @@
   home.sessionVariables = {
     EDITOR = "vim";
     DOTNET_ROOT = "${pkgs.dotnet-sdk_8}";
+    LSP_USE_PLISTS= "true";
   };
 
   # # Enable lorri for easy development environment
