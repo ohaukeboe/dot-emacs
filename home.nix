3eb71d633dc45a9a03b0652f3ce7afb6d182c92b
Oskar Haukebøe <ohaukeboe@pm.me>
2025-02-17 15:47:39 +0100

    font fix again

--- home.nix
+++ home.nix
@@ -232,8 +232,6 @@
     nixgl.nixVulkanIntel
     # nixgl.auto.nixVulkanNvidia
 
-    roboto-mono
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
