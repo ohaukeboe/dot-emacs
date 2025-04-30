6261a8db0f09a8113716957a6f48f8b0b06fa531
Oskar Haukebøe <ohaukeboe@pm.me>
2025-04-30 20:22:01 +0200

    nixfmt

--- flake.nix
+++ flake.nix
@@ -23,47 +23,56 @@
 
   };
 
-  outputs = { self, nixpkgs, home-manager, emacs-overlay, nixgl, mac-app-util, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      emacs-overlay,
+      nixgl,
+      mac-app-util,
+      ...
+    }:
     let
       defaultConfig =
-        if builtins.match ".*darwin" builtins.currentSystem != null
-        then "oskar-darwin"
-        else "oskar";
+        if builtins.match ".*darwin" builtins.currentSystem != null then "oskar-darwin" else "oskar";
 
       # Function to create home-manager configuration for a system
-      mkHomeConfiguration = system:
+      mkHomeConfiguration =
+        system:
         let
           pkgs = import nixpkgs {
             inherit system;
             overlays = [
               emacs-overlay.overlay
               # Only add nixGL overlay for Linux
-              (final: prev: if builtins.match ".*linux" system != null
-                            then (nixgl.overlay final prev)
-                            else {})
+              (final: prev: if builtins.match ".*linux" system != null then (nixgl.overlay final prev) else { })
             ];
           };
           lib = nixpkgs.lib;
         in
-          home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-            # Specify your home configuration modules here, for example,
-            # the path to your home.nix.
-            modules = [
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules =
+            [
               ./home.nix
-            ]++ lib.optionals (builtins.match ".*darwin" system != null) [
+            ]
+            ++ lib.optionals (builtins.match ".*darwin" system != null) [
               mac-app-util.homeManagerModules.default
             ];
 
-            # Optionally use extraSpecialArgs
-            # to pass through arguments to home.nix
-            extraSpecialArgs = {
-              isLinux = builtins.match ".*linux" system != null;
-              isDarwin = builtins.match ".*darwin" system != null;
-            };
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+          extraSpecialArgs = {
+            isLinux = builtins.match ".*linux" system != null;
+            isDarwin = builtins.match ".*darwin" system != null;
           };
-    in {
+        };
+    in
+    {
       homeConfigurations = {
         "oskar" = mkHomeConfiguration "x86_64-linux";
         "oskar-darwin" = mkHomeConfiguration "aarch64-darwin";
