093841e93e1dc9b7d98f8658591c291dcd91cd5d
Oskar Haukeboe <ohaukeboe@pm.me>
2025-05-02 10:37:52 +0200

    fix loading config

--- init.el
+++ init.el
@@ -1,26 +1,22 @@
 ;; Emacs init file responsible for either loading a pre-compiled configuration
 ;; file or tangling and loading a literate org configuration file.
 
+;; Because nix home-manager uses symlinks and sets the changed time
+;; for the actual file being pointed at to 0 (unix epoch) we have to
+;; make sure to read the time of the symlink itself
+
 ;; Don't attempt to find/apply special file handlers to files loaded during
 ;; startup.
-(let ((file-name-handler-alist nil))
-  ;; If config is pre-compiled, then load that
-  (if (file-exists-p (expand-file-name "config.elc" user-emacs-directory))
-      (load-file (expand-file-name "config.elc" user-emacs-directory))
-    ;; Otherwise use org-babel to tangle and load the configuration
-    (require 'org)
-    (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
- '(package-vc-selected-packages
-   '((consult-mu :url "https://github.com/armindarvish/consult-mu.git"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(let ((file-name-handler-alist nil)
+      (config-org (expand-file-name "config.org" user-emacs-directory))
+      (config-elc (expand-file-name "config.el" user-emacs-directory)))
+  (let* ((org-time (nth 5 (file-attributes config-org 'integer)))
+         (elc-time (and (file-exists-p config-elc)
+                        (nth 5 (file-attributes config-elc 'integer)))))
+    ;; If config is pre-compiled, then load that
+    (if (and elc-time
+             (time-less-p org-time elc-time))
+        (load-file config-elc)
+      ;; Otherwise use org-babel to tangle and load the configuration
+      (require 'org)
+      (org-babel-load-file config-org))))
