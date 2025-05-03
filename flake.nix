b248e8a8fb05b530d0452c9fb1039af53d62ccb3
Oskar Haukebøe <ohaukeboe@pm.me>
2025-05-03 20:49:15 +0200

    caches in own file

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
       nixgl,
       mac-app-util,
       ...
-    }@inputs:
+    }:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -79,19 +79,7 @@
           modules =
             [
               ./home.nix
-              {
-                nix.package = pkgs.nix;
-                nix.settings = {
-                  substituters = [
-                    "https://cache.nixos.org"
-                    "https://nix-community.cachix.org"
-                  ];
-                  trusted-public-keys = [
-                    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-                    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                  ];
-                };
-              }
+              ./common/caches.nix
             ]
             ++ lib.optionals (builtins.match ".*darwin" system != null) [
               mac-app-util.homeManagerModules.default
@@ -105,18 +93,7 @@
       nixosConfigurations = {
         x1laptop = nixpkgs.lib.nixosSystem {
           modules = [
-            {
-              nix.settings = {
-                substituters = [
-                  "https://cosmic.cachix.org/"
-                  "https://nix-community.cachix.org"
-                ];
-                trusted-public-keys = [
-                  "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-                  "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                ];
-              };
-            }
+            ./common/caches.nix
             nixos-cosmic.nixosModules.default
             ./configuration.nix
 
