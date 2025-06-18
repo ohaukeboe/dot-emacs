4372fcf1f52ce23c9906d970476062fbe2fc6002
Oskar Haukebøe <ohaukeboe@pm.me>
2025-06-18 20:58:20 +0200

    support TPM unlock of luks encrypted device

--- common/secure-boot.nix
+++ common/secure-boot.nix
@@ -2,11 +2,16 @@
 
 # It is necessary to run `sudo sbctl create-keys` first to generate
 # the secure boot keys
+#
+# To automatically decrypt a disk using TPM, run:
+# `systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=0+7 /dev/<my encrypted device>`
 {
-  environment.systemPackages = [
-    pkgs.sbctl
+  environment.systemPackages = with pkgs; [
+    sbctl
+    tpm2-tss
   ];
 
+  boot.initrd.systemd.enable = true;
   boot.loader.systemd-boot.enable = lib.mkForce false;
 
   boot.lanzaboote = {
