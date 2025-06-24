5878f90eca788770fd54eaff1b752c70d57757a0
Oskar Haukebøe <ohaukeboe@pm.me>
2025-06-24 10:41:37 +0200

    use gpu sync mode on work laptop

--- machines/work-laptop/config.nix
+++ machines/work-laptop/config.nix
@@ -9,9 +9,9 @@
   hardware.nvidia.open = true;
 
   hardware.nvidia.prime = {
-    offload.enable = true;
-    offload.enableOffloadCmd = true;
-    # sync.enable = true;
+    # offload.enable = true;
+    # offload.enableOffloadCmd = true;
+    sync.enable = true;
 
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
