6c9e353fe76943262c91c92ce7b4d3838892c57f
Oskar Haukebøe <ohaukeboe@pm.me>
2025-06-25 08:40:30 +0200

    try different nvidia settings

--- machines/work-laptop/config.nix
+++ machines/work-laptop/config.nix
@@ -11,19 +11,29 @@
     "modesetting"
     "nvidia"
   ];
-  hardware.nvidia.open = true;
-  hardware.nvidia.powerManagement.finegrained = true;
-  hardware.nvidia.prime = {
-    offload.enable = true;
-    offload.enableOffloadCmd = true;
-    # sync.enable = true;
+  hardware.nvidia = {
+    open = true;
 
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = true;
+
+    powerManagement.finegrained = true;
+    modesetting.enable = true;
+    prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = true;
+      # sync.enable = true;
+
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 
   environment.variables = {
-    VDPAU_DRIVER = lib.mkIf config.hardware.opengl.enable (lib.mkDefault "va_gl");
+    VDPAU_DRIVER = lib.mkIf config.hardware.graphics.enable (lib.mkDefault "va_gl");
   };
 
   hardware.graphics.extraPackages = with pkgs; [
