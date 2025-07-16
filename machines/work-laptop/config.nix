1811a05c20047f12a94ede2d479b51a89525f167
Oskar Haukebøe <ohaukeboe@pm.me>
2025-07-16 11:44:01 +0200

    work-laptop gpu changes

--- machines/work-laptop/config.nix
+++ machines/work-laptop/config.nix
@@ -7,21 +7,18 @@
 
 {
   hardware.graphics.enable = true;
-  services.xserver.videoDrivers = [
-    "modesetting"
-    "nvidia"
-  ];
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    open = false;
+    open = true;
+    modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    # powerManagement.enable = true;
+    powerManagement.enable = true;
 
-    # powerManagement.finegrained = true;
-    modesetting.enable = true;
+    powerManagement.finegrained = true;
     prime = {
       # offload.enable = true;
       # offload.enableOffloadCmd = true;
@@ -32,8 +29,12 @@
     };
   };
 
+  boot.kernelParams = lib.mkDefault [
+    "nvidia-drm.modeset=1"
+  ];
+
   environment.variables = {
-    VDPAU_DRIVER = lib.mkIf config.hardware.graphics.enable (lib.mkDefault "va_gl");
+    VDPAU_DRIVER = lib.mkIf config.hardware.graphics.enable (lib.mkDefault "nvidia");
   };
 
   hardware.graphics.extraPackages = with pkgs; [
