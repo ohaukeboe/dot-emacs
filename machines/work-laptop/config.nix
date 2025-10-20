7a089796efd666d31a460d2daf528067a086a4c9
Oskar Haukebøe <ohaukeboe@pm.me>
2025-10-20 09:25:03 +0200

    use reverse sync on work laptop

--- machines/work-laptop/config.nix
+++ machines/work-laptop/config.nix
@@ -22,7 +22,8 @@
     prime = {
       # offload.enable = true;
       # offload.enableOffloadCmd = true;
-      sync.enable = true;
+      # sync.enable = true;
+      reverseSync.enable = true;
 
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
