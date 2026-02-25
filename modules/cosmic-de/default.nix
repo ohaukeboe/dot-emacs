83ce273728fc69e7007e50c3edbe33522d6bb35f
Oskar Haukebøe <ohaukeboe@pm.me>
2026-02-25 10:27:58 +0100

    feat(cosmic-de): Make gtk3 apps use correct theme

--- modules/cosmic-de/default.nix
+++ modules/cosmic-de/default.nix
@@ -27,6 +27,7 @@ in
     environment.sessionVariables = {
       QT_QPA_PLATFORMTHEME = "cosmic";
       COSMIC_DATA_CONTROL_ENABLED = 1; # Clipboard management
+      GTK_THEME = "adw-gtk3-dark";
     };
 
     # XDG Desktop Portal configuration for COSMIC
@@ -38,6 +39,14 @@ in
     # COSMIC-specific packages
     environment.systemPackages = with pkgs; [
       inputs.cutecosmic.packages.${pkgs.stdenv.hostPlatform.system}.default
+      adw-gtk3
     ];
+
+    home-manager.users.${config.user.username} = {
+      services.flatpak.packages = [
+        "org.gtk.Gtk3theme.adw-gtk3"
+        "org.gtk.Gtk3theme.adw-gtk3-dark"
+      ];
+    };
   };
 }
